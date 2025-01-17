942945621e907a5696d53579103be1e3b23dc355
yashvani05 <yashvani1305@gmail.com>
2025-01-17 23:49:07 +0530

    remove extra code

--- modules/auth.controller.js
+++ modules/auth.controller.js
@@ -91,32 +91,6 @@ const customerLogin = async (req, res) => {
     });
 };
 
-// const verifyLogin = async (req, res) => {
-//   try {
-//     const { token } = req.query;
-
-//     if (!token) {
-//       return res.status(400).json({ message: "Token is missing" });
-//     }
-
-//     // Verify token
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     // Find the user and update emailVerified status
-//     const user = await userModelTable.findById(decoded.id);
-//     if (!user) {
-//       return res.status(400).json({ message: "Invalid token" });
-//     }
-
-//     user.emailVerified = true;
-//     await user.save();
-
-//     res.status(200).json({ message: "Email verified successfully!" });
-//   } catch (error) {
-//     res.status(400).json({ message: "Invalid or expired token" });
-//   }
-// };
-
 const verifyLogin = (req, res) => {
   const token = req.query;
   if (!token) {
